5c4a3dd56fba8eb9b2e2ed12b73e452a15221793
celli33 <limon.martinez.misael@gmail.com>
2022-02-19 00:23:52 -0600

    fix: fiel test folder

---
+++ tests/unit/request-builder/fiel-request-builder/fiel.spec.ts
@@ -0,0 +1,35 @@
+import { Fiel } from '../../../../src';
+import { TestCase } from '../../../testcase';
+
+describe('Fiel', () => {
+
+    test('fiel with correct password', () => {
+        const fiel = TestCase.createFielUsingTestingFiles();
+        expect(fiel.isValid()).toBeTruthy();
+    });
+    test('fiel unprotected pem', () => {
+        const fiel = Fiel.create(TestCase.fileContents('fake-fiel/EKU9003173C9.cer'), TestCase.fileContents('fake-fiel/EKU9003173C9.key.pem'), '');
+        expect(fiel.isValid()).toBeTruthy();
+    });
+    test('fiel creating from content', () => {
+        const fiel = Fiel.create(
+            TestCase.fileContents('fake-fiel/EKU9003173C9.cer'),
+            TestCase.fileContents('fake-fiel/EKU9003173C9.key'),
+            TestCase.fileContents('fake-fiel/EKU9003173C9-password.txt').trim(),
+        );
+        expect(fiel.isValid()).toBeTruthy();
+    });
+    test('fiel with incorrect password create an exception ', () => {
+        expect(() => { TestCase.createFielUsingTestingFiles('wrong password') }).toThrow(new Error('Cannot open private key: malformed plain PKCS8 private key(code:001)'));
+    });
+    test('is not valid using CSD', () => {
+        const fiel = Fiel.create(
+            TestCase.fileContents('fake-csd/EKU9003173C9.cer'),
+            TestCase.fileContents('fake-csd/EKU9003173C9.key'),
+            TestCase.fileContents('fake-csd/EKU9003173C9-password.txt').trim(),
+        );
+        expect(fiel.isValid()).toBeFalsy();
+    });
+
+
+});
