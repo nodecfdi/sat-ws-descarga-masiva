286db1da63ed10c5d078829eec9867b09aa4e427
celli33 <limon.martinez.misael@gmail.com>
2022-02-22 21:48:56 -0600

    feat: added package reader exceptions

---
+++ src/package-reader/exceptions/create-temporary-file-zip-exception.ts
@@ -0,0 +1,20 @@
+import { PackageReaderException } from "./package-reader-exception";
+
+export class CreateTemporaryZipFileException extends PackageReaderException {
+
+    private readonly _previous?: Error;
+
+    constructor(message: string, previous?: Error) {
+        super(message);
+        this._previous = previous;
+    }
+
+    public static create(message: string, previous?: Error): CreateTemporaryZipFileException {
+        const messageToSend = previous && previous.message != '' ? `${message} : ${previous.message}` : message;
+        return new CreateTemporaryZipFileException(messageToSend, previous )
+    }
+
+    public getPrevious(): Error | undefined {
+        return this._previous;
+    }
+}
---
+++ src/package-reader/exceptions/package-reader-exception.ts
@@ -0,0 +1,4 @@
+export abstract class PackageReaderException extends Error {
+
+    
+}
