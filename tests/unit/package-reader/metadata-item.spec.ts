050de7f9a090ba1ad019ab389c056eddd1048fed
celli33 <limon.martinez.misael@gmail.com>
2022-02-24 17:46:25 -0600

    test: metadata item

---
+++ tests/unit/package-reader/metadata-item.spec.ts
@@ -0,0 +1,36 @@
+import { MetadataItem } from "../../../src/package-reader/metadata-item";
+import { MetadaPackageReader } from "../../../src/package-reader/metadata-package-reader";
+import { TestCase } from "../../test-case";
+describe('metadata item', () => {
+    test('with empty data', () => {
+        const metadata = new MetadataItem({});
+        expect(metadata.get('uuid')).toBe('');
+        expect(Array.from(metadata.values())).toEqual([]);
+        expect(metadata.all()).toStrictEqual({});
+    });
+
+    test('with contents', () => {
+        const data = { uuid: 'x-uuid', oneData: 'one data' };
+        const metadata = new MetadataItem(data);
+        expect(metadata.get('uuid')).toBe('x-uuid');
+        expect(metadata.get('oneData')).toBe('one data');
+        expect(metadata.all()).toStrictEqual(data);
+    });
+
+    test('reader cfdi in zip', async () => {
+        let expectedContent = TestCase.fileContents('zip/metadata.txt');
+        const zipFileName = TestCase.filePath('zip/metadata.zip');
+        const packageReader = await MetadaPackageReader.createFromFile(zipFileName);
+        const content = await packageReader.fileContents();
+        const [key] = content.keys()
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        let extracted = content.get(key)!;
+
+
+        // normalize line endings
+        expectedContent = expectedContent.replace(new RegExp(/\r\n/, 'g'), "\n");
+        extracted = extracted.replace(new RegExp(/\r\n/, 'g'), "\n");
+
+        expect(extracted).toBe(expectedContent);
+    });
+});
