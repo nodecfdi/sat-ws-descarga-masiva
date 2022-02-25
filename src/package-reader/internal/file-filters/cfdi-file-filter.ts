106f96a21a5c986dbc34294556d5bfc5fe1fe802
celli33 <limon.martinez.misael@gmail.com>
2022-02-24 19:55:06 -0600

    feat: added cfdi file filter

--- src/package-reader/internal/file-filters/cfdi-file-filter.ts
+++ src/package-reader/internal/file-filters/cfdi-file-filter.ts
@@ -1,11 +1,12 @@
-// import { FileFilterInterface } from "./file-filter-interface";
-// export class CfdiFileFilter implements FileFilterInterface {
+import { CfdiPackageReader } from "../../cfdi-package-reader";
+import { FileFilterInterface } from "./file-filter-interface";
+export class CfdiFileFilter implements FileFilterInterface {
 
-//     public filterFilename(filename: string): boolean {
-//         return /^[^\/\\\\]+\.xml/i.test(filename);
-//     }
+    public filterFilename(filename: string): boolean {
+        return /^[^/\\\\]+\.xml/i.test(filename);
+    }
 
-//     public filterContents(contents: string): boolean {
-//         return false;
-//     }
-// }
+    public filterContents(contents: string): boolean {
+        return '' != CfdiPackageReader.obtainUuidFromXmlCfdi(contents);
+    }
+}
