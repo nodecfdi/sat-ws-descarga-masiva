400eedf65157756a53d5063cb3c555fadd228c3a
celli33 <limon.martinez.misael@gmail.com>
2022-02-25 21:18:14 -0600

    test: interacts xml trait

---
+++ tests/unit/internal/interacts-xml-trait-override-specimen.ts
@@ -0,0 +1,16 @@
+import { use } from 'typescript-mix';
+import { InteractsXmlTrait } from '../../../src/internal/interacts-xml-trait';
+import { DOMParser } from '@xmldom/xmldom';
+
+interface InteractsXmlOverrideTraitSpecimen extends InteractsXmlTrait { }
+
+class InteractsXmlOverrideTraitSpecimen {
+    @use(InteractsXmlTrait) private this: unknown;
+
+    public readDocument(source: string): Document {
+        source = '';
+        return new DOMParser().parseFromString(source);
+    }
+}
+
+export { InteractsXmlOverrideTraitSpecimen };
---
+++ tests/unit/internal/interacts-xml-trait.specimen.ts
@@ -0,0 +1,10 @@
+import { use } from 'typescript-mix';
+import { InteractsXmlTrait } from '../../../src/internal/interacts-xml-trait';
+
+interface InteractsXmlTraitSpecimen extends InteractsXmlTrait { }
+
+class InteractsXmlTraitSpecimen {
+    @use(InteractsXmlTrait) private this: unknown;
+}
+
+export { InteractsXmlTraitSpecimen };
