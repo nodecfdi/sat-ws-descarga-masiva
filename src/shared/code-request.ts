d70d7ad2cf7d12c49a30bab2270fad4dd72abae3
celli33 <limon.martinez.misael@gmail.com>
2022-03-04 12:23:09 -0600

    fix: code and status shared

--- src/shared/code-request.ts
+++ src/shared/code-request.ts
@@ -1,35 +1,77 @@
 export class CodeRequest {
-    protected static readonly VALUES = {
-        5000: {
+
+    private index?: number;
+    private value!: { code?: number, name: string, message: string };
+    protected static readonly VALUES = [
+        {
+            code: 5000,
             name: 'Accepted',
             message: 'Solicitud recibida con éxito',
         },
-        5002: {
+        {
+            code: 5002,
             name: 'Exhausted',
             message: 'Se agotó las solicitudes de por vida: Máximo para solicitudes con los mismos parámetros',
-        },
-        5003: {
+        }
+        ,
+        {
+            code: 5003,
             name: 'MaximumLimitReaded',
             message: 'Tope máximo: Indica que se está superando el tope máximo de CFDI o Metadata',
         },
-        5004: {
+        {
+            code: 5004,
             name: 'EmptyResult',
             message: 'No se encontró la información: Indica que no generó paquetes por falta de información.',
         },
-        5005: {
+        {
+            code: 5005,
             name: 'Duplicated',
             message: 'Solicitud duplicada: Si existe una solicitud vigente con los mismos parámetros',
         }
-    };
+    ];
 
-    public static getEntries(): {
-        5000: { name: string, message: string }, 5002: { name: string, message: string }, 5003: { name: string, message: string }, 5004: { name: string, message: string }
-        5005: { name: string, message: string }
-    } {
+    /**
+     * 
+     * @param index if assign by Values.code
+     */
+    constructor(index: number) {
+        if (typeof (index) == 'number') {
+            const value = CodeRequest.VALUES.find(element => index === element.code);
+            if (!value) {
+                this.value = this.getEntryValueOnUndefined();
+                return;
+            };
+            this.value = value;
+            this.index = value.code;
+            return;
+        }
+    }
+
+    public getEntryValueOnUndefined(): { code?: number, name: string, message: string } {
+        return { name: 'Unknown', message: 'Desconocida' };
+    }
+
+    public static getEntries(): { code: number, name: string, message: string }[] {
         return CodeRequest.VALUES;
     }
 
     public getEtryValueOnUndefined(): { name: string, message: string } {
         return { name: 'Unknown', message: 'Desconocida' };
+    }
+
+    public getEntryId(): string {
+        return this.value.name;
+    }
+
+    public getValue(): number | undefined {
+        return this.index;
+    }
+
+    public jsonSerialize(): { value: number | undefined, message: string } {
+        return {
+            value: this.value.code,
+            message: this.value.message
+        };
     }
 }
--- src/shared/status-request.ts
+++ src/shared/status-request.ts
@@ -1,25 +1,30 @@
 export class StatusRequest {
 
     private index?: number;
-    private value!: { name: string, message: string };
+    private value!: { code?: number, name: string, message: string };
 
     protected static readonly VALUES = [
-        { name: 'Accepted', message: 'Aceptada' },
-        { name: 'InProgress', message: 'En proceso' },
-        { name: 'Finished', message: 'Terminada' },
-        { name: 'Failure', message: 'Error' },
-        { name: 'Rejected', message: 'Rechazada' },
-        { name: 'Expired', message: 'Vencida' },
+        { code: 1, name: 'Accepted', message: 'Aceptada' },
+        { code: 2, name: 'InProgress', message: 'En proceso' },
+        { code: 3, name: 'Finished', message: 'Terminada' },
+        { code: 4, name: 'Failure', message: 'Error' },
+        { code: 5, name: 'Rejected', message: 'Rechazada' },
+        { code: 6, name: 'Expired', message: 'Vencida' },
     ];
 
+    /**
+     * 
+     * @param index if number is send assign value by array index of VALUES, values from 0 to 5 if string is send find value by Values.name
+     */
     constructor(index: number | string) {
         if (typeof (index) == 'number') {
-            if (index >= StatusRequest.VALUES.length) {
-                throw new Error('Index not found');
-            }
-            this.index = index;
-            this.value = StatusRequest.VALUES[index];
-            return;
+            const value = StatusRequest.VALUES.find(element => index === element.code);
+            if (!value) {
+                this.value = this.getEntryValueOnUndefined();
+                return;
+            };
+            this.value = value;
+            this.index = value.code;
         }
         if (typeof (index) == 'string') {
             const value = StatusRequest.VALUES.find(element => index === element.name);
@@ -28,7 +33,7 @@ export class StatusRequest {
                 return;
             };
             this.value = value;
-            this.index = StatusRequest.VALUES.indexOf(value);
+            this.index = value.code;
             return;
         }
     }
@@ -37,7 +42,7 @@ export class StatusRequest {
         return StatusRequest.VALUES;
     }
 
-    public getEntryValueOnUndefined(): { name: string, message: string } {
+    public getEntryValueOnUndefined(): { code?: number, name: string, message: string } {
         return { name: 'Unknown', message: 'Desconocida' };
     }
 
@@ -49,7 +54,7 @@ export class StatusRequest {
         return this.index;
     }
 
-    public jsonSerialez(): { value: number | undefined, message: string } {
+    public jsonSerialize(): { value: number | undefined, message: string } {
         return {
             value: this.index,
             message: this.value.message
