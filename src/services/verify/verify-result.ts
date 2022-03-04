7eabfb510d47e8d3998695a24a27cf96b004a0e4
celli33 <limon.martinez.misael@gmail.com>
2022-03-04 12:22:50 -0600

    feat: added verify translator

---
+++ src/services/verify/verify-result.ts
@@ -0,0 +1,72 @@
+import { CodeRequest } from '../../shared/code-request';
+import { StatusCode } from '../../shared/status-code';
+import { StatusRequest } from '../../shared/status-request';
+
+export class VerifyResult {
+    private _status: StatusCode;
+    private _statusRequest: StatusRequest;
+    private _codeRequest: CodeRequest;
+    private _numberCfdis: number;
+    private _packagesIds: string[];
+
+
+    constructor(statusCode: StatusCode, statusRequest: StatusRequest, codeRequest: CodeRequest, numberCfdis: number, ...packageIds: string[]) {
+        this._status = statusCode;
+        this._statusRequest = statusRequest;
+        this._codeRequest = codeRequest;
+        this._numberCfdis = numberCfdis;
+        this._packagesIds = packageIds;
+    }
+
+    /**
+    * Status of the verification call
+    */
+    public getStatus(): StatusCode {
+        return this._status;
+    }
+
+    /**
+     * Status of the query
+     */
+    public getStatusRequest(): StatusRequest {
+        return this._statusRequest;
+    }
+
+    /**
+     * Code related to the status of the query
+     */
+    public getCodeRequest(): CodeRequest {
+        return this._codeRequest;
+    }
+
+    /**
+     * Number of CFDI given by the query
+     */
+    public getNumberCfdis(): number {
+        return this._numberCfdis;
+    }
+
+    /**
+    * An array containing the package identifications, required to perform the download process
+    */
+    public getPackageIds(): string[] {
+        return this._packagesIds;
+    }
+
+    public countPackages(): number {
+        return this._packagesIds.length;
+    }
+
+    public jsonSerialize(): {
+        status: { code: number, message: string }, codeRequest: { value: number | undefined, message: string }, statusRequest: { value: number | undefined, message: string }
+        numberCfdis: number, packagesIds: string[],
+    } {
+        return {
+            status: this._status.jsonSerialize(),
+            codeRequest: this._codeRequest.jsonSerialize(),
+            statusRequest: this._statusRequest.jsonSerialize(),
+            numberCfdis: this._numberCfdis,
+            packagesIds: this._packagesIds,
+        };
+    }
+}
---
+++ src/services/verify/verify-translator.ts
@@ -0,0 +1,29 @@
+import { use } from 'typescript-mix';
+import { InteractsXmlTrait } from '../../internal/interacts-xml-trait';
+import { RequestBuilderInterface } from '../../request-builder/request-builder-interface';
+import { CodeRequest } from '../../shared/code-request';
+import { StatusCode } from '../../shared/status-code';
+import { StatusRequest } from '../../shared/status-request';
+import { VerifyResult } from './verify-result';
+
+export class VerifyTranslator extends InteractsXmlTrait {
+    @use(InteractsXmlTrait) private this: unknown;
+
+    public createVerifyResultFromSoapResponse(content: string): VerifyResult {
+        const env = this.readXmlElement(content);
+
+        const values = this.findAtrributes(env, ...['body', 'VerificaSolicitudDescargaResponse', 'VerificaSolicitudDescargaResult']);
+        const status = new StatusCode(Number(values['codestatus']) ?? 0, values['mensaje'] ?? '');
+        const statusRequest = new StatusRequest(Number(values['estadosolicitud']) ?? 0);
+        
+        const codeRequest = new CodeRequest(Number(values['codigoestadosolicitud'] ?? 0));
+        const numberCfdis = Number(values['numerocfdis']) ?? 0;
+        const packages = this.findContents(env, ...['body', 'VerificaSolicitudDescargaResponse', 'VerificaSolicitudDescargaResult', 'IdsPaquetes']);
+
+        return new VerifyResult(status, statusRequest, codeRequest, numberCfdis, ...packages);
+    }
+
+    public createSoapRequest(requestBuilder: RequestBuilderInterface, requestId: string): string {
+        return requestBuilder.verify(requestId);
+    }
+}
