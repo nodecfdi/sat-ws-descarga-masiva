92fefade5ff7b9f6845691cce44eb1cf0bac74bf
celli33 <limon.martinez.misael@gmail.com>
2022-03-05 12:49:44 -0600

    feat: export files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-// request-builder/fiel-request-builder
 export * from './request-builder/fiel-request-builder/fiel';
+export * from './request-builder/fiel-request-builder/fiel-request-builder';
+export * from './request-builder/request-builder-interface';
+export * from './request-builder/request-builder-interface';
+export * from './services/query/query-parameters';
+export * from './shared/date-time';
+export * from './shared/date-time-period';
+export * from './shared/download-type';
+export * from './shared/request-type';
+export * from './service';
+export * from './package-reader/metadata-package-reader';
+export * from './package-reader/cfdi-package-reader';
+export * from './internal/helpers';
+export * from './package-reader/exceptions/open-zip-file-exception';
+
