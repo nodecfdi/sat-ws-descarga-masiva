375c8d11584bee938d5472d28194f4fb6b682880
celli33 <limon.martinez.misael@gmail.com>
2022-03-04 23:44:56 -0600

    test: web client missing errors

---
+++ tests/unit/web-client/exceptions/http-client-error.spec.ts
@@ -0,0 +1,15 @@
+import { CRequest } from '../../../../src/web-client/crequest';
+import { CResponse } from '../../../../src/web-client/cresponse';
+import { HttpClientError } from '../../../../src/web-client/exceptions/http-client-error';
+import { WebClientException } from '../../../../src/web-client/exceptions/web-client-exception';
+describe('http client error', () => {
+    test('instance of webclientexception', () => {
+        const exception = new HttpClientError(
+            'message',
+            new CRequest('GET', 'unknown://invalid uri/', '', {}),
+            new CResponse(200, '', {})
+        );
+
+        expect(exception).toBeInstanceOf(WebClientException);
+    });
+});
