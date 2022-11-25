a74a9c032a416489f5777d333a7a2f36063b624b
celli33 <limon.martinez.misael@gmail.com>
2022-11-25 11:40:01 -0600

    fix: sonar lints

--- src/package-reader/internal/file-filters/cfdi-file-filter.ts
+++ src/package-reader/internal/file-filters/cfdi-file-filter.ts
@@ -1,7 +1,7 @@
 import { FileFilterInterface } from './file-filter-interface';
 export class CfdiFileFilter implements FileFilterInterface {
     public filterFilename(filename: string): boolean {
-        return /^[^/\\\\]+\.xml/i.test(filename);
+        return /^[^/\\]+\.xml/i.test(filename);
     }
 
     public filterContents(contents: string): boolean {
@@ -11,8 +11,8 @@ export class CfdiFileFilter implements FileFilterInterface {
     public static obtainUuidFromXmlCfdi(xmlContent: string): string {
         const pattern = /:Complemento.*?:TimbreFiscalDigital.*?UUID="(?<uuid>[-a-zA-Z0-9]{36})"/s;
         const found = xmlContent.match(pattern);
-        if (found && found.groups && found.groups['uuid']) {
-            return found.groups['uuid'].toLowerCase();
+        if (found && found.groups && found.groups.uuid) {
+            return found.groups.uuid.toLowerCase();
         }
 
         return '';
