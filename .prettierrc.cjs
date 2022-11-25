f39144fa0115ddbf4a130b71edad0f55e9744cb7
celli33 <limon.martinez.misael@gmail.com>
2022-11-24 23:00:08 -0600

    chore: default configs

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
     semi: true,
     singleQuote: true,
-    trailingComma: 'none',
-    endOfLine: 'lf',
     quoteProps: 'consistent',
-    printWidth: 120
+    trailingComma: 'none',
+    arrowParens: 'always',
+    endOfLine: 'lf'
 };
