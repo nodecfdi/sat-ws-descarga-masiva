cb8f6e5d73c669f9f4d3f2bfc7a504394249c1a5
celli33 <limon.martinez.misael@gmail.com>
2024-04-27 17:59:53 -0600

    chore: base repo config update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: {
-        globals: true,
-        alias: {
-            src: './src',
-        },
-        coverage: {
-            all: true,
-            provider: 'istanbul',
-            reporter: ['text', 'lcov'],
-            include: ['src/**/*.ts'],
-        },
-        setupFiles: ['./tests/setup-xmldom.ts'],
-        environmentMatchGlobs: [['**/*.browser.test.ts', 'jsdom']],
+  test: {
+    globals: true,
+    coverage: {
+      all: true,
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.ts'],
     },
+  },
 });
