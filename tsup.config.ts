f1da0dcaca5977476b7147a62085b7634cc3c295
celli33 <limon.martinez.misael@gmail.com>
2024-04-27 19:58:21 -0600

    fix: index.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['./index.ts', './src/types.ts'],
+  entry: ['./index.ts'],
   outDir: './dist',
   clean: true,
   format: ['esm', 'cjs'],
